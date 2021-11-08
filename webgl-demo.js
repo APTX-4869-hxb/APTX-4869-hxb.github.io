8a5648b417c8a72728067c9f5f4e65f1dacc839b
APTX-4869-hxb <62132172+APTX-4869-hxb@users.noreply.github.com>
2021-11-08 18:21:19 +0800

    Update webgl-demo.js

--- webgl-demo.js
+++ webgl-demo.js
@@ -69,7 +69,7 @@ function main() {
   // Here's where we call the routine that builds all the
   // objects we'll be drawing.
   const buffers = initBuffers(gl);
-  const texture = loadTexture(gl, 'cubetexture.png');
+  const texture = loadTexture(gl, 'cloud.png');
 
   var then = 0;
 
